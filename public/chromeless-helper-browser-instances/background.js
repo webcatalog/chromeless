d92d5b635a3c3a94a0dfaac2b707887b2130237e
Quang Lam <quang.lam2807@gmail.com>
2022-02-06 02:47:25 -0600

    Fix helper extension prevents opening new window in browser instances (#95)

---
+++ public/chromeless-helper-browser-instances/background.js
@@ -0,0 +1,27 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+const browser = window.browser || window.chrome;
+
+// fix Google prevents signing in because of security concerns
+// https://github.com/webcatalog/webcatalog-app/issues/455
+// https://github.com/meetfranz/franz/issues/1720#issuecomment-566460763
+browser.webRequest.onBeforeSendHeaders.addListener((details) => {
+  const { requestHeaders } = details;
+  const blockingResponse = {};
+
+  for (let i = 0, l = requestHeaders.length; i < l; i += 1) {
+    if (requestHeaders[i].name === 'User-Agent') {
+      // remove Chrome version from user-agent string
+      // allowing the app to work with Google login page
+      requestHeaders[i].value = requestHeaders[i].value.replace(/Chrome\/[\d.]+/, 'Chrome');
+      break;
+    }
+  }
+
+  blockingResponse.requestHeaders = requestHeaders;
+  return blockingResponse;
+}, {
+  urls: ['https://accounts.google.com/*'],
+}, ['requestHeaders', 'blocking']);
